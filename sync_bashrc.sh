9dfdd9922449b7493698ecb81ff40a10bfbf3df9
kun.dong <329009063@qq.com>
2016-03-31 18:27:06 +0800

    link vimrc

--- sync_bashrc.sh
+++ sync_bashrc.sh
@@ -2,10 +2,12 @@
 
 cur_path=`pwd`
 bash_rc_path=${cur_path}/.bashrc
+vim_rc_path=${cur_path}/.vimrc
 
 echo "#current path:$cur_path"
 echo "#bashrc  path:$bash_rc_path"
 ln -sf $bash_rc_path ~/.bashrc
+ln -sf $vim_rc_path  ~/.vimrc
 
 
 # add x priviledge 
